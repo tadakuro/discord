95b7e92f2b7a89e9ff95d0922a3a3e7af68b93dc
tadakuro <149168456+tadakuro@users.noreply.github.com>
2026-07-03 07:02:37 +0800

    Delete utils directory

--- utils/logger.js
+++
@@ -1,9 +0,0 @@
-function timestamp() {
-  return new Date().toISOString();
-}
-
-module.exports = {
-  info: (...args) => console.log(`[${timestamp()}] [INFO]`, ...args),
-  warn: (...args) => console.warn(`[${timestamp()}] [WARN]`, ...args),
-  error: (...args) => console.error(`[${timestamp()}] [ERROR]`, ...args),
-};
