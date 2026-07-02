d771500d3dfc819d81ce4a9411894cde67308a5e
tadakuro <149168456+tadakuro@users.noreply.github.com>
2026-07-03 06:53:28 +0800

    Add files via upload

---
+++ utils/loadEvents.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const logger = require('./logger');
+
+function loadEvents(client) {
+  const eventsPath = path.join(__dirname, '..', 'events');
+  const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith('.js'));
+
+  for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+
+    if (!event.name || typeof event.execute !== 'function') {
+      logger.warn(`Skipping invalid event file: ${file}`);
+      continue;
+    }
+
+    if (event.once) {
+      client.once(event.name, (...args) => event.execute(...args));
+    } else {
+      client.on(event.name, (...args) => event.execute(...args));
+    }
+
+    logger.info(`Loaded event: ${event.name} (${file})`);
+  }
+}
+
+module.exports = { loadEvents };
